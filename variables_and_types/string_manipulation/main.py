b033b2ee0c213363018a81818dc2250d44e2399b
pdimetrie <>
2026-06-05 10:38:22 +0000

    String manipulation

---
+++ variables_and_types/string_manipulation/main.py
@@ -0,0 +1,7 @@
+grocery_items = "milk cheese bread apples oranges chicken"
+dairy1 = grocery_items[0:4]
+dairy2 = grocery_items[5:11]
+bakery1 = grocery_items[12:17]
+sentence = dairy1 + " " + dairy2 + " " + bakery1
+
+print(sentence)
